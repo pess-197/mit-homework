616bc745c582b4dabd8d8f7e9839b192b39832e7
Andrei Zakharkin <bigblackdog@gmail.com>
2026-04-02 20:44:43 +0400

    replaced "Hello World!" with recursive Fibonacci function

--- git/main.cpp
+++ git/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+int fib(int n) {
+	if (n == 1) {
+		return 1;
+	}
+	if (n == 2) {
+		return 1;
+	}
+	if (n > 2) {
+		return fib(n-1)+fib(n-2);
+	}
+}
+
 int main() {
-	std::cout << "Hello World!" << std::endl;
+	std:cout << fib(10: << std::endl;
 }
