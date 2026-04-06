e6c39773fb7b531ad7332a465f92d315e05519c6
pess-197 <bigblackdog197@gmail.com>
2026-04-06 14:50:06 +0400

    transfer main.cpp 3.0

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void fib(int n) {
+	int* fibarr = new int[n];
+	fibarr[0] = 0;
+	fibarr[1] = 1;
+	std::cout << 0 << '\n' << 1 << '\n';
+	for (int i = 2; i < n; i++) {
+		fibarr[i] = fibarr[i-1]+fibarr[i-2];
+		std::cout << fibarr[i] << '\n';
+	}
+	delete[] fibarr;
+}
+
+int main() {
+	int n;
+	std::cout << "Enter n: ";
+	std::cin >> n;
+	fib(n);
+	std::cout << "hello world again";
+}
