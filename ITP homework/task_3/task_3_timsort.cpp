a38c197077d5230ede9c7c7b29623c1ffecd8c58
pess-197 <bigblackdog197@gmail.com>
2026-04-24 08:34:27 +0400

    Add insertsort

--- ITP homework/task_3/task_3_timsort.cpp
+++ ITP homework/task_3/task_3_timsort.cpp
@@ -1,3 +1,30 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+void insertsort(vector<int>& vec) {
+    if(vec[0] > vec[1]) {
+        swap(vec[0], vec[1]);
+    }
+    int jj, tmp;
+    for (int i = 2; i < vec.size(); i++) {
+        jj = i;
+        for (int j = 0; j < i; j++) {
+            if (vec[i] < vec[j]) {
+                jj = j;
+                break;
+            }
+        }
+        tmp = vec[i];
+        vec.erase(find(vec.begin(), vec.end(), vec[i]));
+        vec.insert(vec.begin()+jj, tmp);
+    }
+}
+
+int main() {
+    vector<int> vec = {1, 3, 4,2 ,5, 3,52 , 0};
+    insertsort(vec);
+    for (auto &i : vec) {
+        cout << i << " ";
+    }
+}
