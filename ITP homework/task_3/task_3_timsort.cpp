08cc9f1e9ecb8bec7fa9d41ae30c8aa7cb4dec74
pess-197 <bigblackdog197@gmail.com>
2026-04-28 16:50:45 +0400

    Fix some issues

--- ITP homework/task_3/task_3_timsort.cpp
+++ ITP homework/task_3/task_3_timsort.cpp
@@ -16,9 +16,11 @@ void insertsort(vector<int>& vec) {
                     break;
                 }
             }
-            tmp = vec[i];
-            vec.erase(find(vec.begin(), vec.end(), vec[i]));
-            vec.insert(vec.begin()+jj, tmp);
+            if (jj != i) {
+                tmp = vec[i];
+                vec.erase(find(vec.begin(), vec.end(), vec[i]));
+                vec.insert(vec.begin()+jj, tmp);
+            }
         }
     }
 }
@@ -52,6 +54,7 @@ vector<int> timsort(vector<int> &vec) {
     vector<int> run;
     run.push_back(vec[0]);
     for (int i = 0; i < vec.size()-1; i++) {
+        cout << vec[i] << " " << vec[i+1] <<  endl;
         run.push_back(vec[i+1]);
         if (isAscending == 0) {
             if (vec[i] <= vec[i+1]) {
@@ -59,21 +62,17 @@ vector<int> timsort(vector<int> &vec) {
             }
             else isAscending = -1;
         }
+        
         else if (isAscending == 1) {
             if (vec[i] > vec[i+1]) {
                 insertsort(run);
                 runs.push_back(run);
-                for (auto &i : run) {
-                    cout << i << " ";
-                }
-                cout << endl;
                 while (!run.empty()) {
-                    //cout << "h";
                     run.pop_back();
                 }
-                i = i+2;
+                i++;
                 if (i < vec.size()) {
-                    run.push_back(vec[i]);
+                    run.push_back(vec[i+1]);
                 }
                 isAscending = 0;
             }
@@ -82,42 +81,53 @@ vector<int> timsort(vector<int> &vec) {
             if (vec[i] <= vec[i+1]) {
                 insertsort(run);
                 runs.push_back(run);
-                for (auto &i : vec) {
-                    cout << i << " ";
-                }
-                cout << '/n';
                 while (!run.empty()) {
-                    //cout << "h";
                     run.pop_back();
                 }
-                i = i+2;
+                i++;
                 if (i < vec.size()) {
-                    run.push_back(vec[i]);
+                    run.push_back(vec[i+1]);
                 }
                 isAscending = 0;
             }
         }
     }
+    if (!run.empty()) {
+        runs.push_back(run);
+    }
     insertsort(runs);
-    // for (int i = 0; i < runs.size(); i++) {
-    //     for (int j = 0; j < runs[i].size(); j++) {
-    //         for (int k = 0; k < runs[i+1].size(); k++) {
-    //             if (runs[i][j] < runs[i+1][k]) {
-    //                 if (k > 0) {
-    //                     runs[i+1].insert(runs[i+1].begin()+k-1, runs[i][j]);
-    //                 }
-    //                 else  runs[i+1].insert(runs[i+1].begin(), runs[i][j]);
-    //             }
-    //         }
-    //     }
-    // }
+    for (auto &i : runs) {
+        for (auto &j : i) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < runs.size()-1; i++) {
+        for (int j = 0; j < runs[i].size(); j++) {
+            for (int k = 0; k < runs[i+1].size(); k++) {
+                if (runs[i][j] < runs[i+1][k] || k == runs[i+1].size()-1) {
+                    if (k > 0) {
+                        runs[i+1].insert(runs[i+1].begin()+k, runs[i][j]);
+                    }
+                    else  runs[i+1].insert(runs[i+1].begin(), runs[i][j]);
+                    for (auto &j : runs[i+1]) {
+                        cout << j << " ";
+                    }
+                    cout << endl;
+                    break;
+                }
+            }
+        }
+    }
     return runs[runs.size()-1];
 }
 
 int main() {
     vector<int> vec = {1, 3, 4,2 ,5, 3,52 , 0};
+    vector<int> vec2 = {5, 3,52};
+    insertsort(vec2);
     timsort(vec);
-    // for (auto &i : vec) {
-    //     cout << i << " ";
-    // }
+    for (auto &i : vec) {
+        cout << i << " ";
+    }
 }
