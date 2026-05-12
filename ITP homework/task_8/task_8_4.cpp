83b6a95bdbd65c19df1554347c3d3d6990bbd26c
pess-197 <bigblackdog197@gmail.com>
2026-05-12 22:29:10 +0400

    Finish task_8_4.cpp

--- ITP homework/task_8/task_8_4.cpp
+++ ITP homework/task_8/task_8_4.cpp
@@ -3,6 +3,10 @@
 #include <set>
 using namespace std;
 
+vector<int> used;
+vector<int> pr;
+set<vector<int>> cycles_sort;
+
 struct stack {
 	int val;
 	stack* next;
@@ -101,58 +105,129 @@ void dfs(vector<vector<int>> gr, vector<int> a, int x) {
 
 }
 
-set<vector<int>> cycle_search(vector<vector<int>> gr,int a) {
-    vector<int> used(gr.size(),0);
-    set<vector<int>> sett;    
-    used[a] = 2;
-    stack* h = NULL;
-	stack* pr = NULL;
-	stack* pr2 = NULL;
-	int prpr = 0;
-    int ii = 0;
-    int fl = 0;
-    push(h, a);
-	push(pr,a);
-	push(pr2,a);
-    while (h) {
-        a = h->val;
-		cout << a << "," << pr->val << "," << pr2->val << " ";
-		fl = 0;
-        for (int i = 0; i < gr[a].size(); i++) {
-			if (used[gr[a][i]] == 0) {
-				used[gr[a][i]] = 2;
-                ii = i;
-                fl = 1;
-				push(pr2,a);
-				break;
-			}
-			if (used[gr[a][i]] == 1 && gr[a][i] != pr->val && gr[a][i] != pr2->val) {
-				used[gr[a][i]] = 2;
-                ii = i;
-                fl = 1;
-				push(pr2,a);
-				break;
-			}
-			// else if (used[gr[a][i]] == 1 &&  (gr[a][i] == pr->val || gr[a][i] == prpr->val)) {
-			// 	fl = 2;
-			// 	break;
-			// }
-            if (gr[a][i] != pr->val && gr[a][i] != pr2->val  && used[gr[a][i]] == 2) {
-                sett.insert(used);
+// set<vector<int>> cycle_search(vector<vector<int>> gr,int a) {
+//     vector<int> used(gr.size(),0);
+//     set<vector<int>> sett;    
+//     used[a] = 2;
+//     stack* h = NULL;
+// 	stack* pr = NULL;
+// 	stack* pr2 = NULL;
+// 	int prpr = a;
+//     int ii = 0;
+//     int fl = 0;
+//     push(h, a);
+// 	push(pr,a);
+// 	push(pr2,a);
+//     while (h) {
+//         a = h->val;
+// 		//cout << a << ", " << pr->val << ", " << pr2->val << " ";
+// 		cout << a << " ";
+// 		fl = 0;
+//         for (int i = 0; i < gr[a].size(); i++) {
+// 			if (used[gr[a][i]] == 0) {
+// 				used[gr[a][i]] = 2;
+//                 ii = i;
+//                 fl = 1;
+// 				push(pr2,a);
+// 				break;
+// 			}
+// 			if (used[gr[a][i]] == 1 && gr[a][i] != pr->val && gr[a][i] != pr2->val) {
+// 				used[gr[a][i]] = 2;
+//                 ii = i;
+//                 fl = 1;
+// 				push(pr2,a);
+// 				break;
+// 			}
+// 			else if (used[gr[a][i]] == 1 &&  (gr[a][i] == pr->val || gr[a][i] == pr2->val) && used[a] < 3) {
+// 				fl = 2;
+// 				break;
+// 			}
+//             // if (gr[a][i] != pr->val && gr[a][i] != pr2->val  && used[gr[a][i]] == 2) {
+//             //     stack* tmp = 0;
+// 			// 	cout << prpr << " ";
+// 			// 	while (h) {
+// 			// 		push(tmp,h->val);
+// 			// 		cout << h->val << " ";
+// 			// 		pop(h);
+// 			// 	}
+// 			// 	cout << '\n';
+// 			// 	reverse(tmp);
+// 			// 	h = tmp;
+//             // }
+// 		}
+// 		push(pr,a);
+//         if (fl == 1) {
+// 			push(h, gr[a][ii]);
+// 		}
+// 		else if (fl == 0) {
+//             pop(h);
+// 			pop(pr2);
+//             used[a] = 1;
+//         }
+// 		else {
+// 			pop(h);
+// 			pop(pr2);
+//             used[a] = 3;
+// 		}
+//     }
+// 	cout << '\n';
+// 	for (auto &i : used) {
+// 		cout << i << " ";
+// 	}
+//     return sett;
+// }
+
+void insertsort(vector<int>& vec) {
+    if(vec[0] > vec[1]) {
+        swap(vec[0], vec[1]);
+    }
+    if (vec.size() > 2) {
+        int jj, tmp;
+        for (int i = 2; i < vec.size(); i++) {
+            jj = i;
+            for (int j = 0; j < i; j++) {
+                if (vec[i] < vec[j]) {
+                    jj = j;
+                    break;
+                }
             }
-		}
-		push(pr,a);
-        if (fl == 1) {
-			push(h, gr[a][ii]);
-		}
-		else {
-            pop(h);
-			prpr = pr->val;
-			pop(pr2);
-            used[a] = 1;
+            if (jj != i) {
+                tmp = vec[i];
+                vec.erase(vec.begin()+i);
+                vec.insert(vec.begin()+jj, tmp);
+            }
         }
     }
-    return sett;
+}
+
+
+void add_cycle(int sv, int fv) {
+	int curv = fv;
+	vector<int> tmp;
+	while (curv != sv) {
+		tmp.push_back(curv);
+		curv = pr[curv];
+	}
+	tmp.push_back(sv);
+	insertsort(tmp);
+	cycles_sort.insert(tmp);
+}
+
+void cycle_search(int x,vector<vector<int>> gr) {
+	used[x] = 1;
+	for (int i = 0; i < gr[x].size(); i++) {
+		if (pr[x] == gr[x][i]) {
+			continue;
+		}
+		if (used[gr[x][i]] == 0) {
+			pr[gr[x][i]] = x;
+			cycle_search(gr[x][i], gr);
+		}
+		else {
+			add_cycle(gr[x][i],x);
+		}
+	}
+	used[x] =0;
 }
 
 void bfs(vector<vector<int>> gr, vector<int> a, int x) {
@@ -210,12 +285,16 @@ int main() {
 	vector<vector<int>> gr = { {1,2,4,5}, {0,3},{0,5},{1},{0,6},{0,2,6}, {4,5} };
     vector<vector<int>> ribs = { {0,1}, {0,2}, {0,4},{0,5}, {1,3}, {2,5}, {4,6}, {5,6}};
     gr= l_Adj(gr.size(), ribs, false);
-    set<vector<int>> sett = cycle_search(gr,0);
-    for (auto &i : sett) {
+	pr.resize(gr.size());
+	used.resize(gr.size());
+	for (int i = 0; i <gr.size(); i++) {
+		pr[i] = -1;
+		used[i] = 0;
+	}
+	cycle_search(0,gr);
+    for (auto &i : cycles_sort) {
         for (int j = 0; j < i.size(); j++) {
-            if (i[j] == 2) {
-                cout << j << " ";
-            }
+            cout << i[j] << " ";
         }
         cout << '\n';
     }
