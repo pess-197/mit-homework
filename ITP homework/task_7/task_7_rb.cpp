e6bc3cf4f1300a4c25f27596f50e94cf881d4dc9
pess-197 <bigblackdog197@gmail.com>
2026-05-31 22:54:03 +0400

    Finish RBT

--- ITP homework/task_7/task_7_rb.cpp
+++ ITP homework/task_7/task_7_rb.cpp
@@ -10,13 +10,22 @@ struct tree{
     tree* parent;
 };
 
+tree* nil;
+
+void init_nil() {
+    nil = new tree;
+    nil->color = 'b';
+    nil->left = nil->right = nil->parent = nil;
+}
+
+
 tree* root(int x) {
     tree* r = new tree;
     r->color = 'b';
     r->val = x;
-    r->left = NULL;
-    r->right = NULL;
-    r->parent = NULL;
+    r->left = nil; 
+    r->right = nil;
+    r->parent = nil;
     return r;
 }
 
@@ -24,63 +33,63 @@ tree* node(tree* &tr, int x) {
     tree* r = new tree;
     r->color = 'r';
     r->val = x;
-    r->left = NULL;
-    r->right = NULL;
+    r->left = nil;
+    r->right = nil;
     r->parent = tr;
     return r;
 }
 
 tree* lturn(tree* &tr, tree* &x) {
+    if (x == nil) return tr;
     tree* y = x->right;
     x->right = y->left;
-    if (y->left->left || y->left->right) {
+    if (y->left != nil) {               
         y->left->parent = x;
     }
     y->parent = x->parent;
-    if (x->parent->left == x) {
+    if (x->parent == nil) {
+        tr = y;
+    }
+    else if (x == x->parent->left) {
         x->parent->left = y;
     }
     else x->parent->right = y;
     y->left = x;
     x->parent = y;
-    if (!y->parent) {
-        y->color = 'b';
-        return y;
-    }
-    return tr;
+    return tr; 
 }
 
 tree* rturn(tree* &tr, tree* &x) {
+    if (x == nil) return tr;
     tree* y = x->left;
     x->left = y->right;
-    if (y->right->right || y->right->left) {
+    if (y->right != nil) {               
         y->right->parent = x;
     }
     y->parent = x->parent;
-    if (x->parent->left == x) {
-        x->parent->left = y;
+    if (x->parent == nil) {
+        tr = y;
     }
-    else x->parent->right = y;
+    else if (x == x->parent->right) {
+        x->parent->right = y;
+    }
+    else x->parent->left = y;
     y->right = x;
     x->parent = y;
-    if (!y->parent) {
-        y->color = 'b';
-        return y;
-    }
-    return tr;
+    return tr; 
 }
 
 tree* grprnt (tree* &x) {
-    if (x && x->parent) {
+    if (x != nil && x->parent != nil) {
         return x->parent->parent;
     }
-    else return NULL;
+    else return nil;
 }
 
 tree* uncle(tree* &x) {
     tree* g = grprnt(x);
-    if (!g) {
-        return NULL;
+    if (g == nil) {
+        return nil;
     }
     else if (g->right == x->parent) {
         return g->left;
@@ -89,24 +98,24 @@ tree* uncle(tree* &x) {
 }
 
 tree* sblng (tree* &x) {
-    if (x && x->parent) {
+    if (x != nil && x->parent != nil) {
         if (x->parent->left == x) {
             return x->parent->right;
         }
         else return x->parent->left;
     }
-    else return NULL;
+    else return nil;
 }
 
 tree* min(tree* tr) {
-    if (!tr->left) {
+    if (tr->left == nil) {
         return tr;
     }
     else return min(tr->left);
 }
 
 tree* max(tree* tr) {
-    if (!tr->right) {
+    if (tr->right == nil) {
         return tr;
     }
     else return max(tr->right);
@@ -126,23 +135,26 @@ void delete_case_6(tree* &tr, tree* &x);
 
 void insert_case_5(tree* &tr, tree* &x) {
     tree* g = grprnt(x);
-    x->parent->color = 'b';
-    g->color = 'r';
-    if (x->parent->left == x && g->left == x->parent) {
-        rturn(tr,g);
+    if (x != nil && x->parent != nil && g != nil) {
+        x->parent->color = 'b';
+        g->color = 'r';
+        if (x->parent->left == x && g->left == x->parent) {
+            rturn(tr,g);
+        }
+        else if (x->parent->right == x && g->right == x->parent) lturn(tr,g);
     }
-    else lturn(tr,g);
 }
 
 void insert_case_4(tree* &tr, tree* &x) {
     tree* g = grprnt(x);
+    if (g == nil) return;
     if (x->parent->right == x && g->left == x->parent) {
-        lturn(tr,x);
+        lturn(tr,x->parent);
         x=x->left;
     }
     else {
         if (x->parent->left == x && g->right == x->parent) {
-            rturn(tr,x);
+            rturn(tr,x->parent);
             x=x->right;
         }
     }
@@ -152,42 +164,43 @@ void insert_case_4(tree* &tr, tree* &x) {
 void insert_case_3(tree* &tr, tree* &x) {
     tree* u = uncle(x);
     tree* g = grprnt(x);
-    if (u && u->color == 'r' && x->parent->color == 'r') {
-        x->parent->color = 'b';
-        u->color = 'b';
-        g->color = 'r';
-        insert_case_1(tr,g);
+    if (g !=nil) {
+        if (u != nil && u->color == 'r' && x->parent->color == 'r') {
+            x->parent->color = 'b';
+            u->color = 'b';
+            g->color = 'r';
+            insert_case_1(tr,g);
+        }
+        else insert_case_4(tr,x);
     }
-    else insert_case_4(tr,x);
 }
 
 void insert_case_2(tree* &tr, tree* &x) {
-    if (x->color = 'r') {
+    if (x->parent->color == 'r') {
         insert_case_3(tr,x);
     }
-    else return;
+    else {
+        return;
+    }
 }
 
 void insert_case_1(tree* &tr, tree* &x) {
-    if (!x->parent) {
+    if (x->parent == nil) {
         x->color = 'b';
     }
     else insert_case_2(tr,x);
 }
 
 void delete_case_1(tree* &tr, tree* &x) {
-    if (!x->parent) {
-        if (x->left) {
-            tr = x->left;
-        }
-        else tr = x->right;
+    if ( x != nil && x->parent == nil) {
+        return;
     }
     else delete_case_2(tr,x);
 }
 
 void delete_case_2(tree* &tr, tree* &x) {
     tree* s = sblng(x);
-    if (s->color == 'r') {
+    if (s !=nil && s->color == 'r') {
         x->parent->color = 'r';
         s->color = 'b';
         if (x->parent->left == x) {
@@ -200,7 +213,7 @@ void delete_case_2(tree* &tr, tree* &x) {
 
 void delete_case_3(tree* &tr, tree* &x) {
     tree* s = sblng(x);
-    if (x->parent->color == 'b' && s->color == 'b' && (s->left->color == 'b' || (!s->left->left && !s->left->right)) && (s->right->color == 'b' || (!s->right->left && !s->right->right))) {
+    if (x != nil && x->parent != nil && s != nil && x->parent->color == 'b' && s->color == 'b' && s->right && s->left != nil && s->left->color == 'b' && s->right->color == 'b') {
         s->color = 'r';
         delete_case_1(tr,x);
     }
@@ -209,7 +222,7 @@ void delete_case_3(tree* &tr, tree* &x) {
 
 void delete_case_4(tree* &tr, tree* &x) {
     tree* s = sblng(x);
-    if (x->parent->color == 'r' && s->color == 'b' && (s->left->color == 'b' || (!s->left->left && !s->left->right)) && (s->right->color == 'b' || (!s->right->left && !s->right->right))) {
+    if (x != nil && x->parent != nil && x->parent->color == 'r' && s != nil && s->color == 'b' && s->right && s->left != nil && s->left->color == 'b' && s->right->color == 'b') {
         s->color = 'r';
         x->parent->color = 'r';
     }
@@ -218,115 +231,147 @@ void delete_case_4(tree* &tr, tree* &x) {
 
 void delete_case_5(tree* &tr, tree* &x) {
     tree* s = sblng(x);
-    if (x->parent->left == x && (s->left->color == 'r' && (s->left->left || s->left->right)) && (s->right->color == 'b' || (!s->right->left && !s->right->right))) {
+    if (s == nil) {
+        delete_case_6(tr, x);
+        return;
+    }
+    if (x != nil && x->parent->left == x && s->right && s->left != nil && s->right->color == 'b' && s->left->color == 'r') {
         s->color = 'r';
         s->left->color = 'b';
-        lturn(tr,s);
-    }
-    else if (x->parent->right == x && (s->right->color == 'r' && (s->right->left || s->right->right)) && (s->left->color == 'b' || (!s->left->left && !s->left->right))) {
+        rturn(tr, s);
+    } 
+    else if (x != nil && x->parent->right == x && s->left != nil && s->right != nil && s->left->color == 'b' && s->right->color == 'r') {
         s->color = 'r';
         s->right->color = 'b';
-        rturn(tr,s);
+        lturn(tr, s);
     }
-    delete_case_6(tr,x);
+    delete_case_6(tr, x);
 }
 
 void delete_case_6(tree* &tr, tree* &x) {
     tree* s = sblng(x);
-    s->color=s->parent->color;
-    s->parent->color = 'b';
-    if (x->parent->left == x) {
-        s->right->color = 'b';
+    if (s != nil) {
+        s->color=s->parent->color;
+        s->parent->color = 'b';
+    }
+    if (x != nil && x->parent->left == x) {
+        if (s->right != nil) s->right->color = 'b';
         lturn(tr,x->parent);
     }
     else {
-        s->left->color = 'b';
+        if (x != nil && s->left != nil) s->left->color = 'b';
         rturn(tr,x->parent);
     }
 }
 
 void insert(tree* &tr, tree* &pr, int x) {
-    if (x < pr->val && !pr->left) {
-        pr->left = node(pr,x);
-        insert_case_1(tr,pr->left);
+    if (x < pr->val && pr->left == nil) {
+        pr->left = node(pr, x);
+        insert_case_1(tr, pr->left);
+    }
+    else if (x > pr->val && pr->right == nil) {
+        pr->right = node(pr, x);
+        insert_case_1(tr, pr->right);
     }
     else {
-        if (x > pr->val && !pr->right) {
-            pr->right = node(pr,x);
-            insert_case_1(tr,pr->right);
+        if (x < pr->val && pr->left != nil) {
+            insert(tr, pr->left, x);
         }
-        else {
-            if (x < pr->val && pr->left) {
-                insert(tr,pr->left,x);
-            }
-            if (x > pr->val && pr->right) {
-                insert(tr,pr->right,x);
-            }
+        if (x > pr->val && pr->right != nil) {
+            insert(tr, pr->right, x);
         }
     }
 }
 
-void replace(tree* &tr, tree* &x) {
-    if (x->left) {
-        tree* ch = x->left;
-        ch->parent = x->parent;
-        if (x->parent) {
-            if (x->parent->left == x) {
-                x->left = ch;
-            }
-            else x->right = ch;
-        }
-    }
-    else {
-        tree* ch = x->right;
-        ch->parent = x->parent;
-        if (x->parent) {
-            if (x->parent->right == x) {
-                x->right = ch;
-            }
-            else x->left = ch;
-        }
-    }
+void replace(tree* &tr, tree* &u, tree* &v) {
+    if (u->parent == nil)
+        tr = v;
+    else if (u == u->parent->left)
+        u->parent->left = v;
+    else
+        u->parent->right = v;
+    if (v != nil)
+        v->parent = u->parent;
 }
 
 void del(tree* &tr, tree*&x) {
-    tree* tmp;
-    tree* t;
-    if (x->left && x->right) {
-        if (x->val <= tr->val) {
-            tmp = max(x);
-        }
-        else tmp = min(x);
-        t->val = x->val;
-        x->val = tmp->val;
-        tmp->val = t->val;
-        x = tmp;
+    if (x == nil) return;                   
+    tree* y = x;
+    tree* child;
+    char y_original_color = y->color;
+    if (x->left == nil) {
+        child = x->right;
+        replace(tr, x, x->right);
     }
-    if (x->left || x->right) {
-        if (x->left && !x->right) {
-            t= x->left;
-        }
-        if (x->right && !x->left) {
-            t= x->right;
-        }
-        replace(tr,x);
-        if (x->color == 'b') {
-            if (t->color == 'r') {
-                t->color = 'b';
-            }
-            else delete_case_1(tr,x);
-        }
+    else if (x->right == nil) {
+        child = x->left;
+        replace(tr, x, x->left);
     }
     else {
-        if (x->color == 'b') {
-            delete_case_1(tr,x);
+        y = max(x->left);
+        y_original_color = y->color;
+        child = y->left;                   
+        if (y->parent == x) {
+            child->parent = y;             
         }
         else {
-            if (x->parent->left == x) {
-                x->parent->left = NULL;
-            }
-            else x->parent->right = NULL;
+            replace(tr, y, y->left);
+            y->left = x->left;
+            y->left->parent = y;
+        }
+        replace(tr, x, y);
+        y->right = x->right;
+        y->right->parent = y;
+        y->color = x->color;
+    }
+
+    if (y_original_color == 'b') {
+        if ( child != nil && child->color == 'r') child->color = 'b';
+        else {
+            delete_case_1(tr, child);
         }
     }
     delete x;
+    x = nil;                              
+}
+
+void inorder(tree* &tr) {
+    if (tr != nil) {
+        inorder(tr->left);
+        cout << tr->val <<tr->color <<  ", ";
+        // if (tr->left) cout << tr->left->val<<tr->left->color  << ", ";
+        // if (tr->right) cout << tr->right->val<<tr->right->color  << ", ";
+        //cout << endl;
+        inorder(tr->right);
+    }
+}
+
+void del_odd(tree* &tr, tree* &x) {
+    if (x == nil) return;
+    del_odd(tr, x->left);
+    del_odd(tr,x->right);
+    if (x->val % 2 != 0) {
+        tree* tmp = x;
+        del(tr, tmp);
+    }
+}
+
+int main() { //task #12
+    init_nil();
+       
+    int n;
+    cout << "enter n:";
+    cin >> n;
+    tree* tr;
+    int val;
+    cin >> val;
+    tr = root(val);
+    for (int i = 1; i <n; i++) {
+        cin >> val;
+        insert(tr,tr,val);
+    }
+    inorder(tr);
+    cout << endl;
+    del_odd(tr,tr);
+    inorder(tr);
 }
